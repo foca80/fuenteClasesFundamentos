720d054e7cd8905165613779c5581217ffd352ed
flores.orihuela <flores.orihuela@gmail.com>
2020-06-20 13:26:24 -0500

    first commit

---
+++ repetitivas_1.rb
@@ -0,0 +1,7 @@
+=begin 
+   Desarrollar un programa tal que sabiendo que se tienen las notas
+   de los alumnos de un curso se pueda saber el primedio de notas 
+   de  toda del curso y la cantidad de desaprobados que existen 
+=end
+
+nota= [20, 28, 19, 15, 12, 16, 16]
