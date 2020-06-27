ee77a636e0de3e7768c1365ef48b329950fcb619
flores.orihuela <flores.orihuela@gmail.com>
2020-06-27 18:55:10 -0500

    3.00

--- arreglosEjemplo3.rb
+++ arreglosEjemplo3.rb
@@ -1,10 +1,13 @@
 =begin 
-Se tienen 2 arreglos de números,
-
+Se tienen 2 arreglos de números.
 Escribir un programa que permita:
 
-1.	Crear un nuevo arreglo con los valores del primer arreglo que se encuentran en el segundo arreglo. 
+1.	Crear un nuevo arreglo con los valores del primer arreglo que se encuentran 
+    en el segundo arreglo. 
 2.	Contar la cantidad de valores del arreglo obtenido en la pregunta anterior.
+
+*Considerar que no se repiten los elementos en un mismo arreglo.
+
 =end
 
 def creanuevoArreglo(arreglo1,arreglo2)
@@ -15,7 +18,7 @@ def cuentaValores(arreglo1,arreglo2)
 	
 end
 
-@arregloA=[10,8,13,15,9,12,]
+@arregloA=[10,8,13,15,9,12,3]   
 @arregloB=[3,5,9,15,20]
 @arregloC=[2,7,5,6]
 @arregloD=[]
