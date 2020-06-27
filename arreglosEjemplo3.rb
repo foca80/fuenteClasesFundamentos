3fff56f080349008291a392a23e3012ddc7651d8
flores.orihuela <flores.orihuela@gmail.com>
2020-06-27 18:49:12 -0500

    4.00

---
+++ arreglosEjemplo3.rb
@@ -0,0 +1,24 @@
+=begin 
+Se tienen 2 arreglos de números,
+
+Escribir un programa que permita:
+
+1.	Crear un nuevo arreglo con los valores del primer arreglo que se encuentran en el segundo arreglo. 
+2.	Contar la cantidad de valores del arreglo obtenido en la pregunta anterior.
+=end
+
+def creanuevoArreglo(arreglo1,arreglo2)
+ 
+end
+
+def cuentaValores(arreglo1,arreglo2)
+	
+end
+
+@arregloA=[10,8,13,15,9,12,]
+@arregloB=[3,5,9,15,20]
+@arregloC=[2,7,5,6]
+@arregloD=[]
+
+puts creanuevoArreglo(@arregloA,@arregloB)) 
+puts cuentaValores(@arregloA,@arregloB)) 
--- arreglosEx1.rb
+++ arreglosEx1.rb
@@ -5,9 +5,9 @@
  satisfacción en cuanto a la atención. El cliente debe digitar en el teléfono el número
  que se le indica y las respuestas que puede dar son las siguientes:
 Concepto:
-Excelente 5
-Muy Bueno 4
-Bueno     3
+Excelente 5  => >=5
+Muy Bueno 4  => >=4 u <5
+Bueno     3  => >=3 y <4
 Regular   2
 Malo      1 
  
@@ -21,11 +21,27 @@ b)	Desarrollar un subprograma que permita buscar los colaboradores que han
 =end
 
 def obtenerPromedio(encuestas)
- 
+   suma = 0.0
+   n = encuestas.length - 1
+   for i in 0..n
+     suma = suma + encuestas[i]
+   end
+   return suma/encuestas.length
 end
 
 def obtenerMejores(puntajes,agentes)
- 
+     contadorExcelentes = 0
+     contadorBuenos = 0
+     arregloTemporal = []
+
+     for i in 0..puntajes.length - 1
+          if puntajes[i]>=5
+           arregloTemporal.push(agentes[i])
+          elsif puntajes[i] >= 4 and puntajes[i] <5
+           arregloTemporal.push(agentes[i])
+          end 
+     end
+     return arregloTemporal
 end
  
  
@@ -35,7 +51,7 @@ def test_obtenerPromedio
   encuestasAgente1 = [5,3,5,3,2]
   encuestasAgente2 = [3,2,3,5,1]
   encuestasAgente3 = [1,5,3,4,5]
- 
+  puts "***** PROMEDIOS *************"
   puts obtenerPromedio(encuestasAgente1)
   puts obtenerPromedio(encuestasAgente2)
   puts obtenerPromedio(encuestasAgente3)
@@ -43,14 +59,16 @@ end
  
  
  def test_obtenerMejores
- 
+  
   analistas = ["Margarita Garriazo","Sandra Chumpitaz","Juan Palomino","Alicia Enriquez","Juan Mascaro"]
   puntaje1 = [3.6,1.4,4.5,2.2,1]
   puntaje2 = [2.3,1.2,4.3,2.5,4.1]
   puntaje3 = [4.1,1.2,3.2,4.7,3.5]
-                   	
+  puts "*********** MEJORES ****************"
 	puts obtenerMejores(puntaje1, analistas) #arreglo
+    puts "*********** MEJORES ****************" 
 	puts obtenerMejores(puntaje2, analistas)
+    puts "*********** MEJORES ****************"
 	puts obtenerMejores(puntaje3, analistas)
         	
  end  
